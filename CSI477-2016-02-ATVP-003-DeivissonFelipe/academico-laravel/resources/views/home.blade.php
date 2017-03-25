83f320372dc02716cbf115741b351cf49af5c7c5
DeivissonFelipe <deivisson48@yahoo.com.br>
2017-03-25 10:02:01 -0300

    Submissao de ATV03 (Academico-Laravel)

---
+++ CSI477-2016-02-ATVP-003-DeivissonFelipe/academico-laravel/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+@section('conteudo')
+<br><br>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Sistema Acadêmico</div>
+
+            <div class="panel-body">
+                Sistema de cadastro de alunos, disciplinas, turma e todo controle administrativo estudantil.
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
